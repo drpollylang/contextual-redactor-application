c948823e6b9112c7accdc8729ce0793cc479dc08
Polly Lang <polly.lang@somerset.gov.uk>
2026-02-06 16:00:17 +0000

    Registered new api functions related to projects

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,6 +5,9 @@ import "./functions/getUploadSas";  // your SAS upload function
 import "./functions/getDownloadSas";// your SAS download function
 import "./functions/listUserDocuments";// list docs in Blob Storage for given userId/projectId
 import "./functions/startRedaction";// calls the redaction-backend-func Functions App to initialise and enact the AI suggestions pipeline
+import "./functions/listProjects"; // lists all projects under files/userId/ from Blob Storage
+import "./functions/createProjectFolders"; // creates a project folder in Blob storage
+import "./functions/deleteProjectFolders"; // deletes a project folder in Blob storage (moves it to /discarded/, which is deleted every 30 days unless it is edited)
 
 app.setup({
     enableHttpStream: true,
